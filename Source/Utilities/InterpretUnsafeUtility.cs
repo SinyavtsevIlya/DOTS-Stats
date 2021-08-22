3b321836019ca9792eddbe563e14ece25bd4c824
Ilya Sinyavtsev <sinyavtsevilya@gmail.com>
2021-08-23 00:57:50 +0300

    package added
    plus some difene fixes

--- Source/Utilities/InterpretUnsafeUtility.cs
+++ Source/Utilities/InterpretUnsafeUtility.cs
@@ -7,13 +7,21 @@ namespace Nanory.Unity.Entities.Unsafe
     {
         public static ref TRetrieve Retrieve<TInput, TRetrieve>(ref TInput inst) where TInput : struct where TRetrieve : struct
         {
+#if ENTITIES_011_2
             return ref UnsafeUtilityEx.As<TInput, TRetrieve>(ref inst);
+#else
+            return ref UnsafeUtility.As<TInput, TRetrieve>(ref inst);
+#endif
         }
 
         public static ref TRetrieve Retrieve<TInput, TRetrieve>(ref TInput inst, int offset) where TInput : struct where TRetrieve : struct
         {
             var pt = UnsafeUtility.AddressOf(ref inst);
+#if ENTITIES_011_2
             return ref UnsafeUtilityEx.AsRef<TRetrieve>(IntPtr.Add((IntPtr) pt, offset).ToPointer());
+#else
+            return ref UnsafeUtility.AsRef<TRetrieve>(IntPtr.Add((IntPtr) pt, offset).ToPointer());
+#endif
         }
     }
 }
